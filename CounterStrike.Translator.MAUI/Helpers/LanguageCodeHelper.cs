c7f98147ee226b50f5399961c6bbb7f8f9c7514e
Tuomas Savonius <tuomas.savonius@receptum.fi>
2023-07-29 21:04:59 +0300

    Fixed a few deadlocks

--- CounterStrike.Translator.MAUI/Helpers/LanguageCodeHelper.cs
+++ CounterStrike.Translator.MAUI/Helpers/LanguageCodeHelper.cs
@@ -9,25 +9,14 @@ public static class LanguageCodeHelper
 {
     private static readonly HashSet<string> ValidLanguageCodes = new()
     {
-        "aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av",
-        "ay", "az", "ba", "be", "bg", "bh", "bi", "bm", "bn", "bo",
-        "br", "bs", "ca", "ce", "ch", "co", "cr", "cs", "cu", "cv",
-        "cy", "da", "de", "dv", "dz", "ee", "el", "en", "eo", "es",
-        "et", "eu", "fa", "ff", "fi", "fj", "fo", "fr", "fy", "ga",
-        "gd", "gl", "gn", "gu", "gv", "ha", "he", "hi", "ho", "hr",
-        "ht", "hu", "hy", "hz", "ia", "id", "ie", "ig", "ii", "ik",
-        "io", "is", "it", "iu", "ja", "jv", "ka", "kg", "ki", "kj",
-        "kk", "kl", "km", "kn", "ko", "kr", "ks", "ku", "kv", "kw",
-        "ky", "la", "lb", "lg", "li", "ln", "lo", "lt", "lu", "lv",
-        "mg", "mh", "mi", "mk", "ml", "mn", "mr", "ms", "mt", "my",
-        "na", "nb", "nd", "ne", "ng", "nl", "nn", "no", "nr", "nv",
-        "ny", "oc", "oj", "om", "or", "os", "pa", "pi", "pl", "ps",
-        "pt", "qu", "rm", "rn", "ro", "ru", "rw", "sa", "sc", "sd",
-        "se", "sg", "si", "sk", "sl", "sm", "sn", "so", "sq", "sr",
-        "ss", "st", "su", "sv", "sw", "ta", "te", "tg", "th", "ti",
-        "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty", "ug",
-        "uk", "ur", "uz", "ve", "vi", "vo", "wa", "wo", "xh", "yi",
-        "yo", "za", "zh", "zu"
+        "af", "ak", "am", "ar", "as", "ay", "az", "be", "bg", "bho", "bm", "bn", "bs", "ca", "ceb", "ckb", "co", "cs", "cy",
+        "da", "de", "doi", "dv", "ee", "el", "en", "eo", "es", "et", "eu", "fa", "fi", "fil", "fr", "fy", "ga", "gd", "gl",
+        "gn", "gom", "gu", "ha", "haw", "he", "hi", "hmn", "hr", "ht", "hu", "hy", "id", "ig", "ilo", "is", "it", "ja", "jv",
+        "ka", "kk", "km", "kn", "ko", "kri", "ku", "ky", "la", "lb", "lg", "ln", "lo", "lt", "lus", "lv", "mai", "mg", "mi",
+        "mk", "ml", "mn", "mni-Mtei", "mr", "ms", "mt", "my", "ne", "nl", "no", "nso", "ny", "om", "or", "pa", "pl", "ps", "pt",
+        "qu", "ro", "ru", "rw", "sa", "sd", "si", "sk", "sl", "sm", "sn", "so", "sq", "sr", "st", "su", "sv", "sw", "ta",
+        "te", "tg", "th", "ti", "tk", "tl", "tr", "ts", "tt", "ug", "uk", "ur", "uz", "vi", "xh", "yi", "yo", "zh-CN",
+        "zh-TW", "zu"
     };
 
     public static bool IsLanguageCodeValid(string languageCode)
